f9a03c4ac8dfb5ee3c47aa666ce4dfad84e5be53
Muhammed Arshaq <arshaq31@gmail.com>
2025-06-06 21:24:32 +0530

    add: centralized error handling middleware

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { config } from "../config/config";
 import { connectDB } from "../infrastructure/database/db";
+import { errorHandler } from "../interface/middlewares/error.middleware";
 
 const app: Application = express();
 const PORT = config.PORT;
@@ -18,8 +19,12 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "API is working" });
 });
 
+//error handling middleware
+app.use(errorHandler)
+
 //listen port
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  //connect to Database
   connectDB()
 });

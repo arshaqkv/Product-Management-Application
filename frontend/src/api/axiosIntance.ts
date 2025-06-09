fd56ffd0cf527fd73dd553a0ab9f853ddb037fc2
Muhammed Arshaq <arshaq31@gmail.com>
2025-06-09 12:33:48 +0530

    setup: axios configuration

---
+++ frontend/src/api/axiosIntance.ts
@@ -0,0 +1,39 @@
+import { config } from "../config/config";
+import axios from "axios";
+
+const BASE_URL = config.app.PORT;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        window.location.href = "/login";
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
